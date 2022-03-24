4644b46e0cba98907705faae3ac5de3de9a8c6cd
Filipe Aguiar <filipe_aac@yahoo.com.br>
2022-03-24 08:53:17 -0300

    Correções, exemplo env, integração com Prisma

---
+++ prisma/migrations/20220323162551_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Manchete" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "expiration" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
