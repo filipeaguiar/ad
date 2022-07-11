d238575e6802444ce70394f7ef257e96cea98003
filipeaguiar <filipe_aac@yahoo.com.br>
2022-07-11 15:33:42 -0300

    Alteração de consulta de API de Internações

--- src/providers/SQL/internacao.sql
+++ src/providers/SQL/internacao.sql
@@ -6,12 +6,16 @@ SELECT
   (
     internacoes.dthr_alta_medica :: date - internacoes.dthr_internacao :: date
   ) as tempo_ocupacao,
-  leitos.ind_acompanhamento_ccih as ccih
+  internacoes.dthr_alta_medica as data_alta,
+  leitos.ind_acompanhamento_ccih as ccih,
+  observacoes.descricao as observacao
 FROM
   agh.ain_internacoes as internacoes
   LEFT OUTER JOIN agh.agh_especialidades as especialidades ON especialidades.seq = internacoes.esp_seq
   LEFT OUTER JOIN agh.ain_leitos as leitos ON internacoes.lto_lto_id = leitos.lto_id
+  LEFT OUTER JOIN agh.ain_observacoes_censo as observacoes ON observacoes.int_seq = internacoes.seq
 WHERE
   leitos.ind_situacao = 'A'
 ORDER BY
   tempo_ocupacao
+  
