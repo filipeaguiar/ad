ad20d8075c93882f8c99880f646e4a1544e01a1e
filipeaguiar <filipe_aac@yahoo.com.br>
2022-07-13 12:07:02 -0300

    Raça/Cor corrigida

--- src/controllers/bpaController.ts
+++ src/controllers/bpaController.ts
@@ -291,7 +291,7 @@ const BPAiMagnetico = async function (mesAno: String, file: any) {
                     totalProcedimentos: sumBy(16, arrayData),
                     totalPaginas: pageNumber,
                     totalLinhas: totalLinhas,
-                    data,
+                    data: printable,
                     printable
                 })
             })
@@ -366,21 +366,6 @@ const BPAcMagnetico = async function (mesAno: String, file: any) {
 }
 
 export default class bpaController {
-    // /**
-    //  * 
-    //  * @param req Objeto Request do ExpressJS
-    //  * @param res Objeto Response do ExpressJS
-    //  * @param next Objeto que representa o próximo middleware a ser executado
-    //  */
-    // static async getBPA(req: Request, res: Response, next) {
-    //     const { start, end, atendimento } = req.query
-    //     if (!start || !end) {
-    //         res.send(await BPAProvider.getBPA(atendimento))
-    //     }
-    //     else {
-    //         res.send(await BPAProvider.getBPAiByPeriod(start, end))
-    //     }
-    // }
 
     static async getBPAc(req: Request, res: Response, next) {
         const BPAc = await BPAProvider.getBPAc(req.params.mesAno)
