6071eea3ba2ff8bc0e0d8e911f62d1640f02dd90
Filipe Aguiar <filipe_aac@yahoo.com.br>
2024-08-16 18:08:34 -0300

    Remoção de Log

--- src/controllers/materialEstoqueController.ts
+++ src/controllers/materialEstoqueController.ts
@@ -21,15 +21,12 @@ export default class MaterialEstoqueController {
     try {
 
       const master: MaterialMaster[] = await MaterialMasterProvider.getMaterial()
-      console.log(master)
-
       const aghu: Material[] = await MaterialAGHUProvider.getMaterial()
+
       // Caso o item seja OPME, o valor unitário será o valor unitário do master
       const result = aghu.map((item) => {
         const masterItem = master.find((masterItem) => masterItem["Cod Master"] === parseInt(item["Cod Master"]))
         if (item["OPME?"] === "Sim") {
-          console.log(item["Cod Master"])
-          console.log(masterItem)
           return {
             "codigo": item.codigo,
             "nome": item.nome,
